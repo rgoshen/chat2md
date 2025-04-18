bcb9646265a81e440c60375a3ca0eb160038a065
rgoshen <rick.goshen@gmail.com>
2025-04-17 17:45:27 -0700

    fix failing output standard test
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,5 @@ def sample_conversation_path():
 @pytest.fixture
 def sample_conversation_dict(sample_conversation_path):
     with open(sample_conversation_path, "r", encoding="utf-8") as f:
-        return json.load(f)
+        conversations = json.load(f)
+    return { "conversations": conversations }
